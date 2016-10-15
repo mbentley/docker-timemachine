49439cfc69a4cd011bff5e671fd1bf0d6272e40f
Matt Bentley <mbentley@mbentley.net>
2016-10-15 12:26:10 -0400

    Add pid cleanup to entrypoint script
    Signed-off-by: Matt Bentley <mbentley@mbentley.net>

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,28 @@ then
     echo "Using default password: timemachine"
 else
     echo "Setting password from environment variable"
-    echo timemachine:$PASSWORD | chpasswd
+    echo timemachine:${PASSWORD} | chpasswd
+fi
+
+# cleanup dbus PID file
+if [ -f /var/run/dbus/pid ]
+then
+  echo "dbus PID exists; removing..."
+  rm -v /var/run/dbus/pid
+fi
+
+# cleanup netatalk PID file
+if [ -f /var/run/netatalk.pid ]
+then
+  echo "netatalk PID exists; removing..."
+  rm -v /var/run/netatalk.pid
+fi
+
+# cleanup avahi-daemon PID file
+if [ -f /var/run/avahi-daemon/pid ]
+then
+  echo "avahi-daemon PID exists; removing..."
+  rm -v /var/run/avahi-daemon/pid
 fi
 
 # run CMD
