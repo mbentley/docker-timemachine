6e598523ebb05f6fae28c845d1304975858c87f0
Alexis Victor Nicolas Bagia <alexis@bagia.de>
2023-12-09 23:04:54 +0100

    fix indent

--- entrypoint.sh
+++ entrypoint.sh
@@ -249,7 +249,8 @@ then
   fi
   if [ "${IGNORE_DOS_ATTRIBUTES}" == "true" ]
   then
-    echo "store dos attributes = no
+    echo "
+   store dos attributes = no
    map hidden = no
    map system = no
    map archive = no
