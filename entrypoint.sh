43c1abbfc766e490dd72f47299ae790e9a4906da
Alexis Victor Nicolas Bagia <alexis@bagia.de>
2023-12-09 22:45:25 +0100

    fix if, again

--- entrypoint.sh
+++ entrypoint.sh
@@ -247,13 +247,14 @@ then
    fruit:wipe_intentionally_left_blank_rfork = yes
    fruit:delete_empty_adfiles = yes" > /etc/samba/smb.conf
   fi
-    if [ "${IGNORE_DOS_ATTRIBUTES}" == "true" ]
+  if [ "${IGNORE_DOS_ATTRIBUTES}" == "true" ]
+  then
     echo "store dos attributes = no
    map hidden = no
    map system = no
    map archive = no
    map readonly = no" >> /etc/samba/smb.conf
-    fi
+  fi
 
   # mkdir if needed
   createdir /var/lib/samba/private 700
